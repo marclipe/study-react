dc87f1f5dce799acaffef070740fefba666c9cc5
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-04-21 21:46:16 -0300

    Apply css global and css module

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header";
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post
         author="MarcLipe"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque obcaecati incidunt quam vero cupiditate saepe odio, quasi illum omnis."
---
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from '../components/Header.module.css'
+
+export function Header(){
+    return (
+      <header className={styles.header}>
+        <strong>Ignite Feed</strong>
+      </header>
+    );
+}

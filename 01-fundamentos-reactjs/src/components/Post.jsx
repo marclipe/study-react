6cc6c4951d821db104053b43749c2e72730d18ec
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-04-27 20:52:25 -0300

    Comment Form in Post component

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -19,6 +19,7 @@ export function Post() {
             Publicado há 1hr
           </time>
         </header>
+
         <div className={styles.content}>
           <p>Fala galeraa 👋</p>
           <p>
@@ -34,6 +35,18 @@ export function Post() {
             <a href="#">#rocketseat</a>{" "}
           </p>
         </div>
+
+        <form className={styles.commentForm}>
+          <strong>Deixe o seu feedback</strong>
+
+          <textarea
+            placeholder='Deixe um comentário'
+          />
+
+          <footer>
+            <button type='submit'>Publicar</button>
+          </footer>
+        </form>
       </article>
     );
 }

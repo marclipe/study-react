c13a81377bc311b63c2c40518ba0c197e7766350
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-05-30 23:52:13 -0300

    Project with Typescript part 1

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,75 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+import styles from './App.module.css'
+import './global.css'
+
+//author: {avartar_url: "", name: "", role: ""}
+//publishedAt: Date
+//content: string
+
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://img.freepik.com/free-vector/cute-man-working-laptop-with-coffee-cartoon-vector-icon-illustration-people-technology-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3869.jpg?w=740&t=st=1683754315~exp=1683754915~hmac=e119a17e4a0aa1576c802b6cee450050fd5491b244be209f7e9ce9830dd9d12b",
+      name: "MarcLipe",
+      role: "Full Stack Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare🚀",
+      },
+      { type: "link", content: "marc.design/programmer" },
+    ],
+    publishedAt: new Date("2023-05-04 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        "https://img.freepik.com/free-vector/cute-man-working-laptop-with-coffee-cartoon-vector-icon-illustration-people-technology-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3869.jpg?w=740&t=st=1683754315~exp=1683754915~hmac=e119a17e4a0aa1576c802b6cee450050fd5491b244be209f7e9ce9830dd9d12b",
+      name: "Marcos Felipe",
+      role: "Front End Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare🚀",
+      },
+      { type: "link", content: "marc.design/programmer" },
+    ],
+    publishedAt: new Date("2023-05-10 20:00:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {post.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ 01-fundamentos-reactjs-ts/src/components/Post.tsx
@@ -0,0 +1,125 @@
+import { format, formatDistanceToNow} from 'date-fns'; 
+import ptBR from 'date-fns/locale/pt-BR'
+import { FormEvent, useState } from "react";
+
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
+import styles from '../components/Post.module.css'
+
+interface Author {
+  name: string; 
+  role: string; 
+  avatarUrl: string;
+}
+
+interface PostProps {
+  author: Author;
+  publishedAt: Author;
+  content: Author
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein?!'
+  ])
+
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR, 
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment(event: FormEvent) {
+    event.preventDefault(); 
+    
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(event: FormEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity("Este campo é obrigatório!");
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeleteOne = comments.filter(comment => {
+      return comment != commentToDelete
+    })
+    setComments(commentsWithoutDeleteOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
+  return (
+    <article className={styles.post}>
+      <header className={styles.header}>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time
+          className={styles.time}
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+         {publishedDateRelativeToNow} 
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map(line => {
+          if(line.type === 'paragraph'){
+            return <p key={line.content}>{line.content}</p>;
+          } else if(line.type === 'link') {
+            return <p key={line.content}><a href="#">{line.content}</a></p>
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+
+        <textarea 
+          name='comment'
+          placeholder="Deixe um comentário" 
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
+
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map(comment => {
+          return (
+          <Comment 
+            key={comment}
+            content={comment} 
+            onDeleteComment={deleteComment}
+            />
+          );
+        })}
+      </div>
+    </article>
+  );
+}

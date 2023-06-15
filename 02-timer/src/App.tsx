a736ab621e9e38363dc546d8f5134a9c52a6e611
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-06-15 20:00:23 -0300

    Styled Components part 2

--- 02-timer/src/App.tsx
+++ 02-timer/src/App.tsx
@@ -3,11 +3,11 @@ import { Button } from "./components/Button"
 export function App() {
   return (
     <>
-      <Button color="primary" />
-      <Button color="secondary"/>
-      <Button color="success"/>
-      <Button color="danger"/>
-      <Button/>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
     </>
   );
 }

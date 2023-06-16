afb0297a89d3b42f1fe3d0dfae0baeeeb98dbeaa
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-06-15 23:07:36 -0300

    Config Theme

--- 02-timer/src/components/Button.styles.ts
+++ 02-timer/src/components/Button.styles.ts
@@ -16,9 +16,16 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
   
-  ${props => {
-    return css`background-color: ${buttonVariants[props.variant]}`
-  }}
+  /* ${props => {
+    return css`
+      background-color: ${buttonVariants[props.variant]}`
+  }} */
 `;
 

3d6b7daf5f48862b1ce99445ce5f7f0ac2ecad7b
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-06-23 22:23:44 -0300

    Buldind carousel

--- oringin-six-lp/src/components/SectionTestimonials/SectionTestimonials.tsx
+++ oringin-six-lp/src/components/SectionTestimonials/SectionTestimonials.tsx
@@ -4,30 +4,52 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import perfilOne from '../../assets/images/perfil-one.png'
 
+import { Pagination } from 'swiper';
+
 export function SectionTestimonials() {
 
   const slides = [
-    { description: "1", name: "Wanessa Souza", image: perfilOne },
-    { description: "2", name: "Wanessa Souza", image: perfilOne },
-    { description: "3", name: "Wanessa Souza", image: perfilOne},
+    { id: '1', description: "Eu sou cliente do Beautysalon há 5 anos e não troco por nada! Certamente meu cabelo mudou muito depois que comecei a tratar somente com produtos naturais e veganos. São profissionais incríveis e qualificados.", name: "Wanessa Souza", image: perfilOne },
+    { id: '2', description: "Eu sou cliente do Beautysalon há 5 anos e não troco por nada! Certamente meu cabelo mudou muito depois que comecei a tratar somente com produtos naturais e veganos. São profissionais incríveis e qualificados.", name: "Wanessa Souza", image: perfilOne },
+    { id: '3', description: "Eu sou cliente do Beautysalon há 5 anos e não troco por nada! Certamente meu cabelo mudou muito depois que comecei a tratar somente com produtos naturais e veganos. São profissionais incríveis e qualificados.", name: "Wanessa Souza", image: perfilOne},
   ];
 
   return (
     <>
-      <h1 className={styles.title}>Depoimentos de quem já passou por aqui</h1>
+      <h1 className={styles.title}>
+        Depoimentos de quem já <br /> passou por aqui
+      </h1>
 
-      <Swiper>
+      <Swiper
+        slidesPerView={2}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+
+        breakpoints={{
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 1,
+            // spaceBetween: 20
+          },
+          // when window width is >= 768px
+          768: {
+            slidesPerView: 2,
+            // spaceBetween: 40
+          },
+        }}
+      >
         {slides.map((item) => (
-          <SwiperSlide>
-            <div>
-              <p className={styles.description}>
-                {item.description}
-              </p>
-              <img 
-              src={item.image} 
-              alt={item.description} 
-              className={styles.slide__item}/>
-              <p className={styles.name}>{item.name}</p>
+          <SwiperSlide className={styles.container__slider} key={item.id}>
+            <div className={styles.container__slide}>
+              <p className={styles.description}>{item.description}</p>
+              <div className={styles.slide__item}>
+                <img
+                  src={item.image}
+                  alt={item.description}
+                  className={styles.slide__image}
+                />
+                <p className={styles.name}>{item.name}</p>
+              </div>
             </div>
           </SwiperSlide>
         ))}

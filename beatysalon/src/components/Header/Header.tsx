f7fe1d31bfdb1545e49772d759fad85667ca1e9a
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-03 18:32:46 -0300

    Adjunting section Header

--- beatysalon/src/components/Header/Header.tsx
+++ beatysalon/src/components/Header/Header.tsx
@@ -1,44 +1,110 @@
-import { useState } from "react";
-import { Logo } from "../Logo/Logo";
+import { useEffect, useState } from "react";
 import styles from "./Header.module.css";
+import menuIcon from '../../assets/icons/menu-icon.svg'; 
+import menuClose from '../../assets/icons/close-icon.svg';
+import logoMobile from '../../assets/images/logoMobile.png';
 
 export function Header() {
 
 	const [isOpen, setIsOpen] = useState(false)
+	const [isDesktop, setIsDesktop] = useState(false);
 
-	const toggleMenu = () => {
-		setIsOpen(!isOpen)
-	}
+		const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+
+		useEffect(() => {
+			const handleResize = () => {
+				setIsDesktop(window.innerWidth > 768);
+			}; 
+
+			handleResize();
+
+			window.addEventListener('resize', handleResize); 
+
+			return () => {
+				window.removeEventListener("resize", handleResize);
+			}
+		}, []);
 
     return (
-		<header className={styles.header}>
-			<div className={styles.header__logo}>
-				<Logo />
-			</div>
-			<div className={`${styles.header__item} ${isOpen ? styles.open: ''}`}>
-						<div className={styles.menuIcon} onClick={toggleMenu}>
-							<span>ok</span>
-							<span></span>
-							<span></span>
-						</div>
-						<ul className={styles.menu}>
-								<li className={styles.menu__item}>
-										<a className={styles.menu__link} href="#">Início</a>
-								</li>
-								<li className={styles.menu__item}>
-										<a className={styles.menu__link} href="#">Sobre</a>
-								</li>
-								<li className={styles.menu__item}>
-										<a className={styles.menu__link} href="#">Serviços</a>
-								</li>
-								<li className={styles.menu__item}>
-										<a className={styles.menu__link} href="#">Depoimentos</a>
-								</li>
-								<li className={styles.menu__item}>
-										<a className={styles.menu__link} href="#">Contato</a>
-								</li>
-						</ul>
-				</div>
-		</header>
+      <header className={styles.header}>
+        <div
+          className={`${styles.header__logo} ${isOpen ? styles.hideLogo : ""}`}
+        >
+          <a href=""><img className={styles.logoMobile} src={logoMobile} alt="#" /></a>
+        </div>
+        <div>
+          <div
+            className={`${styles.menuIcon} ${isDesktop ? styles.hideMenu : ""}`}
+            onClick={toggleMenu}
+          >
+            {!isOpen && <img src={menuIcon} alt="" />}
+            {isOpen && <img src={menuClose} alt="" />}
+          </div>
+          {isOpen && (
+            <div>
+              <ul className={styles.menu}>
+                <li className={styles.menu__item}>
+                  <a className={styles.menu__link} href="#">
+                    Início
+                  </a>
+                </li>
+                <li className={styles.menu__item}>
+                  <a className={styles.menu__link} href="#">
+                    Sobre
+                  </a>
+                </li>
+                <li className={styles.menu__item}>
+                  <a className={styles.menu__link} href="#">
+                    Serviços
+                  </a>
+                </li>
+                <li className={styles.menu__item}>
+                  <a className={styles.menu__link} href="#">
+                    Depoimentos
+                  </a>
+                </li>
+                <li className={styles.menu__item}>
+                  <a className={styles.menu__link} href="#">
+                    Contato
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        {isDesktop && (
+          <div>
+            <ul className={styles.menu}>
+              <li className={styles.menu__item}>
+                <a className={styles.menu__link} href="#">
+                  Início
+                </a>
+              </li>
+              <li className={styles.menu__item}>
+                <a className={styles.menu__link} href="#">
+                  Sobre
+                </a>
+              </li>
+              <li className={styles.menu__item}>
+                <a className={styles.menu__link} href="#">
+                  Serviços
+                </a>
+              </li>
+              <li className={styles.menu__item}>
+                <a className={styles.menu__link} href="#">
+                  Depoimentos
+                </a>
+              </li>
+              <li className={styles.menu__item}>
+                <a className={styles.menu__link} href="#">
+                  Contato
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </header>
     );
 }

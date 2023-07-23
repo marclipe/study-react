0c3cb1d1fadff6f97ed91899b18e705940ad6a8e
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-23 18:06:44 -0300

    New Cycle Active

--- 02-timer/src/pages/Home/index.tsx
+++ 02-timer/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from 'react'
 
 import {
   CountdownContainer,
@@ -21,7 +22,20 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
 })
 
+// Para definir o formato dos ciclos
+interface Cycle {
+  // Preciso de um id para representar unicamente um ciclo
+  id: string
+  task: string // um ciclo tem um tarefa
+  minutesAmount: number // quant.. de minutos
+}
+
 export function Home() {
+  // Vamos usar estado, para armazenar um novo ciclo no nosso component
+  const [cycles, setCycles] = useState<Cycle[]>([]) // Vou armazenar uma lista de ciclos no meu estado, um array de ciclos
+  // Vamos usar um estado para indicar se o ciclo está ativo ou não
+  const [aciveCycleId, setAciveCycleId] = useState<string | null>(null)
+
   // eslint-disable-next-line no-use-before-define
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -34,9 +48,26 @@ export function Home() {
   type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    // Crio uma variável para facilitar o uso do id
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id, // Vai pegar a data atual em milissegundos, a gente converte ese id para uma string
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    // Preciso copiar todos os ciclos que já tenho
+    setCycles((state) => [...state, newCycle])
+    setAciveCycleId(id) // seto o novo ciclo ativo
+
     reset()
   }
+
+  // Crio uma variável, para retornar o mesmo id do ciclo ativo
+  // ela vai percorrer o Array e vai encontrar o id que seja igual ao id do ciclo ativo
+  const activeCycle = cycles.find((cycles) => cycles.id === aciveCycleId)
+  console.log(activeCycle)
 
   const task = watch('task') // Controlled Component
   const isSubmitDisabled = !task

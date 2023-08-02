ca67b7a5a9c723c2cd1e5f246732cfa055c3b275
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-08-02 20:24:11 -0300

    Interrupt cycle

--- 02-timer/src/pages/Home/index.tsx
+++ 02-timer/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { HandPalm, Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -12,6 +12,7 @@ import {
   MinutesAmoutInput,
   Separator,
   StartCountdownButton,
+  StopCountdownButton,
   TaskInput,
 } from './styles'
 
@@ -28,6 +29,7 @@ interface Cycle {
   task: string
   minutesAmount: number
   startDate: Date
+  interruptedDate?: Date // ela pode existir ou não
 }
 
 export function Home() {
@@ -58,9 +60,9 @@ export function Home() {
     }
 
     return () => {
-      clearInterval(interval) // deletar os intervalos inúteis
+      clearInterval(interval)
     }
-  }, [activeCycle]) // Variável externa
+  }, [activeCycle])
 
   type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
@@ -76,9 +78,24 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setAciveCycleId(id)
-    setAmountSecondsPassed(0) // setar os segundos para 0
+    setAmountSecondsPassed(0)
 
     reset()
+  }
+
+  function handleInterruptCycle() {
+    setCycles(
+      cycles.map((cycle) => {
+        // a nova informação sendo a data atual
+        if (cycle.id === aciveCycleId) {
+          return { ...cycle, interruptedDate: new Date() }
+        } else {
+          return cycle // sem alterações
+        }
+      }),
+    )
+
+    setAciveCycleId(null) /// / Vai setar o ciclo de volta para nulo
   }
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
@@ -93,7 +110,6 @@ export function Home() {
   const seconds = String(secondsAmount).padStart(2, '0')
 
   useEffect(() => {
-    // Só vou executar se o ciclo tiver ativo
     if (activeCycle) {
       document.title = `${minutes}:${seconds}`
     }
@@ -101,6 +117,8 @@ export function Home() {
 
   const task = watch('task') // Controlled Component
   const isSubmitDisabled = !task
+
+  console.log(cycles)
 
   return (
     <HomeContainer>
@@ -111,6 +129,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
             {...register('task')}
           />
 
@@ -128,6 +147,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
@@ -142,10 +162,18 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {/* Para interromper o ciclo */}
+        {activeCycle ? (
+          <StopCountdownButton onClick={handleInterruptCycle} type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   )
--- 02-timer/src/pages/Home/styles.ts
+++ 02-timer/src/pages/Home/styles.ts
@@ -84,7 +84,7 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 `
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -93,20 +93,29 @@ export const StartCountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
 
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
 
-  background-color: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${(props) => props.theme['green-500']};
 
   &:hover:not(:disabled) {
     background-color: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${(props) => props.theme['red-500']};
+
+  &:hover:not(:disabled) {
+    background-color: ${(props) => props.theme['red-700']};
   }
 `

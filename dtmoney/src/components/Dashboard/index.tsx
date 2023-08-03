a2366cef7d736d290e68f5b234bfa3308169eb83
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-08-03 17:33:38 -0300

    Transaction Table Component

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TrasactionsTable } from "../TransactionsTable";
 import { ContainerDashboard } from "./styles";
 
 export function Dashboard() {
   return (
     <ContainerDashboard>
       <Summary/>
+      <TrasactionsTable/>
     </ContainerDashboard>
   )
 }
